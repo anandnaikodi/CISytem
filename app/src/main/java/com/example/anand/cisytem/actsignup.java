b4e0e4e3fe201f9efb5cee96b90c4d17c10e251f
lalith <lalithl1993@gmail.com>
2016-10-14 18:32:18 +0530

    baah test

--- app/src/main/java/com/example/anand/cisytem/actsignup.java
+++ app/src/main/java/com/example/anand/cisytem/actsignup.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -18,20 +19,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public class actsignup extends AppCompatActivity implements View.OnClickListener {
-    private static final String REGISTER_URL = "http://192.168.0.107/CIS/student_signup.php";
+public class actsignup extends AppCompatActivity  implements View.OnClickListener{
+    private static final String REGISTER_URL = "http://192.168.43.53/CIS/student_signup.php";
 
     public static final String KEY_NAME = "name";
     public static final String KEY_EMAIL = "email";
     public static final String KEY_PASSWORD = "password";
+    public static final String KEY_TABLE = "table";
+
+
 
 
 
     private EditText Textname;
     private EditText TextEmail;
+    private EditText TextconPassword;
     private EditText TextPassword;
-
+    private Spinner seluser;
     private Button buttonSubmit;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,14 +47,17 @@ public class actsignup extends AppCompatActivity implements View.OnClickListener
         Textname = (EditText) findViewById(R.id.txt_name);
         TextEmail = (EditText) findViewById(R.id.txt_email);
         TextPassword = (EditText) findViewById(R.id.txt_password);
-
+        TextconPassword = (EditText) findViewById(R.id.txt_repassword);
+        seluser=(Spinner)findViewById(R.id.spinner);
 
         buttonSubmit = (Button) findViewById(R.id.btn_submit);
 
         buttonSubmit.setOnClickListener(this);
     }
 
-    private void registerUser(){
+    private void registerUser(String tablelocal){
+        System.out.println("inside registerUser");
+        final String table=tablelocal;
         final String username = Textname.getText().toString().trim();
         final String email = TextEmail.getText().toString().trim();
         final String password = TextPassword.getText().toString().trim();
@@ -70,6 +81,7 @@ public class actsignup extends AppCompatActivity implements View.OnClickListener
                 params.put(KEY_NAME,username);
                 params.put(KEY_PASSWORD,password);
                 params.put(KEY_EMAIL, email);
+                params.put(KEY_TABLE, table);
                 return params;
             }
 
@@ -81,8 +93,24 @@ public class actsignup extends AppCompatActivity implements View.OnClickListener
 
 
     public void onClick(View v) {
-        if(v == buttonSubmit){
-            registerUser();
+        if(v==buttonSubmit) {
+            if(!TextPassword.getText().toString().equals(TextconPassword.getText().toString()))
+            {
+                System.out.println("inside lkfkj");
+                Toast.makeText(actsignup.this,"password mismatched",Toast.LENGTH_LONG).show();
+                return;
+            }
+            String testing=seluser.getSelectedItem().toString();
+            System.out.println("testing="+testing);
+
+
+            if (testing.equals("Admin")) {
+                System.out.println("inside admin if");
+                registerUser("classrepresentative");
+            } else if (testing.equals("Student")) {
+                System.out.println("inside student if");
+                registerUser("student");
+            }
         }
     }
 }
