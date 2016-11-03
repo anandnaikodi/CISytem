e0434e640a7170a45cb0dfe1a1348d5535855bc8
anand1107 <anandnaikodi1107@gmail.com>
2016-11-03 12:16:26 +0530

    anand 03.11.2016(navigational drawer for admin )

---
+++ app/src/main/java/com/example/anand/cisytem/actadmin_listcategory.java
@@ -0,0 +1,38 @@
+package com.example.anand.cisytem;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by anand on 03-11-2016.
+ */
+
+
+
+/**
+ * Created by Belal on 18/09/16.
+ */
+
+
+public class actadmin_listcategory extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.activity_adminfrag_listcategory, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("List Category");
+    }
+}
