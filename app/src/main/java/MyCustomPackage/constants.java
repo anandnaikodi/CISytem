155d0cae88431ca977a6c87627fd3a99dcce5700
anand1107 <anandnaikodi1107@gmail.com>
2016-12-11 12:04:33 +0530

    ui updates

--- app/src/main/java/MyCustomPackage/constants.java
+++ app/src/main/java/MyCustomPackage/constants.java
@@ -5,7 +5,7 @@ package MyCustomPackage;
  */
 
 public class constants {
-    public static String url="http://192.168.0.108:8080";
+    public static String url="http://192.168.0.110:8080";
     public static int id=0;
     public static int classroom_id=1;
     /*public constants() {
