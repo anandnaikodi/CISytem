bd2b3e9c615e40f332cbb616c9caf7bef39e4b75
anand1107 <anandnaikodi1107@gmail.com>
2016-12-14 09:47:34 +0530

    third internals

--- app/src/main/java/MyCustomPackage/constants.java
+++ app/src/main/java/MyCustomPackage/constants.java
@@ -5,11 +5,10 @@ package MyCustomPackage;
  */
 
 public class constants {
-    public static String url="http://192.168.43.53";
+    public static String url="http://192.168.0.102:8080";
     public static int id;
     public static int classroom_id;
     /*public constants() {
         super();
     }*/
-
 }
